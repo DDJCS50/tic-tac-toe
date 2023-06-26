ab91dee8e0f25716ab4df0aacf55e95d8a5e3ab2
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-26 09:29:20 -0700

    Add starter files
    Added starter files needed for the project and added boilerplate to
    each one. Updated README to display the goal of the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tic-tac-toe
+
+The goal of this project is to create a webpage for playing tic tac toe using the properties of HTML, CSS, and JavaScript.
---
+++ script.js
@@ -0,0 +1,18 @@
+// let myModule = (function() {
+
+//     let _privateProperty = 'Hello World';
+//     let publicProperty = 'I am a public property';
+  
+//     function _privateMethod() {
+//       console.log(_privateProperty);
+//     }
+  
+//     function publicMethod() {
+//       _privateMethod();
+//     }
+  
+//     return {
+//       publicMethod: publicMethod,
+//       publicProperty: publicProperty
+//     };
+//   })();
